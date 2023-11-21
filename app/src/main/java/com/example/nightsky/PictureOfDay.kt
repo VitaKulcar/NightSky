5c249b8b4a43e9a1a08c83a39896adcfea12abf7
Vita <vita.kulcar@gmail.com>
2023-11-21 15:58:49 +0100

    fact of day - dela pridobitev z api

--- app/src/main/java/com/example/nightsky/PictureOfDay.kt
+++ app/src/main/java/com/example/nightsky/PictureOfDay.kt
@@ -1,60 +1,17 @@
 package com.example.nightsky
 
-class PictureOfDay(
-    private var _copyright: String? = null,
-    private var _date: String? = null,
-    private var _explanation: String? = null,
-    private var _hdurl: String? = null,
-    private var _mediaType: String? = null,
-    private var _serviceVersion: String? = null,
-    private var _title: String? = null,
-    private var _url: String? = null
-) {
-    var copyright: String?
-        get() = _copyright
-        set(value) {
-            _copyright = value
-        }
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
-    var date: String?
-        get() = _date
-        set(value) {
-            _date = value
-        }
-
-    var explanation: String?
-        get() = _explanation
-        set(value) {
-            _explanation = value
-        }
-
-    var hdurl: String?
-        get() = _hdurl
-        set(value) {
-            _hdurl = value
-        }
-
-    var mediaType: String?
-        get() = _mediaType
-        set(value) {
-            _mediaType = value
-        }
-
-    var serviceVersion: String?
-        get() = _serviceVersion
-        set(value) {
-            _serviceVersion = value
-        }
-
-    var title: String?
-        get() = _title
-        set(value) {
-            _title = value
-        }
-
+data class PictureOfDay(
+    var copyright: String?,
+    var date: String?,
+    var explanation: String?,
+    var hdurl: String?,
+    var mediaType: String?,
+    var serviceVersion: String?,
+    var title: String?,
     var url: String?
-        get() = _url
-        set(value) {
-            _url = value
-        }
-}
+)
